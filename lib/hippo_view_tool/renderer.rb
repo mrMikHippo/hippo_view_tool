d40553e8eb20649e3d66c713aead08c42db00f73
mrMikOcto <mik.hippo@yandex.ru>
2019-01-29 19:55:56 +0300

    Fixed '-' in copyright

--- lib/hippo_view_tool/renderer.rb
+++ lib/hippo_view_tool/renderer.rb
@@ -1,7 +1,7 @@
 module HippoViewTool
 	class Renderer
 		def self.copyright start_date="", name, msg
-			"&copy; #{start_date + "-" unless start_date.empty?} #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+			"&copy; #{start_date + "-" unless start_date.empty?}#{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
 		end
 	end
 end
